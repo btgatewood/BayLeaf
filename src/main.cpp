037398b7cd1b673e9c39ed1879e546ea54f245a6
Bradley Gatewood <gatewoodbt@outlook.com>
2023-05-03 01:57:49 -0400

    Initial build

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  // using std::cout
+#include <string>
+#include <vector>
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "BayLeaf Engine v0.0.1" << '\n';
+	return 0;
+}
